e1e5f47623a028f8e100cf16fef42afe38df061b
Ajay Singh Rajpoot <ajayrajpoot1993@gmail.com.com>
2022-09-03 11:46:25 +0530

    add log

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
+  console.log("authHeader>>>",req)
   console.log("authHeader>>>",authHeader)
   if (!authHeader) {
     const error = new Error('Not authenticated.');
